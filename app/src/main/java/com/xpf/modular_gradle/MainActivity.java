77bc19f2ca3f97c86ab593d00efcf39139a70d38
jiushu <xpfwork@foxmail.com>
2021-06-30 11:02:37 +0800

    isRelease 变量使用

--- app/src/main/java/com/xpf/modular_gradle/MainActivity.java
+++ app/src/main/java/com/xpf/modular_gradle/MainActivity.java
@@ -7,12 +7,20 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (BuildConfig.DEBUG) {// 这段逻辑在release环境，就不会执行
             Log.d("TAG", "onCreate: ");
+        }
+
+        if (BuildConfig.isRelease) {
+            Log.d(TAG, "当前为: 集成化模式，线上/正式环境，除app可运行，其它子模块扩扩扩都是Android Library");
+        } else {
+            Log.d(TAG, "当前为: 组件化模式，测试/开发环境，所有模块可独立运行");
         }
 
     }
